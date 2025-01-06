c20f2d5dd9a2149f66df2a8597c55459730154d4
VasanthBalasubramaniyan <vambuvasanth124@gmail.com>
2025-01-06 14:58:10 +0530

    Updated navbar and side nav and imported slider images

---
+++ index.js
@@ -0,0 +1,19 @@
+// Offer bar
+var offerClose = document.getElementById("offer-close") 
+offerClose.addEventListener("click",function(){
+    var offerBar = document.querySelector(".offer__bar")
+    offerBar.style.display = "none"
+})
+
+// Sidenav
+var menuIcon = document.getElementById("menuicon")
+var sideNav = document.getElementById("side-nav")
+var sideNavClose = document.getElementById("sideNavClose")
+
+menuIcon.addEventListener("click",function(){
+    sideNav.style.left = "0"
+})
+
+sideNavClose.addEventListener("click",function(){
+    sideNav.style.left = "-50%"
+})
